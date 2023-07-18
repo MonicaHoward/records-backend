544766dab8f2bfc1f0fd469e100b07ad774078e2
Monica <jay.m.howard@gmail.com>
2023-07-17 21:05:26 -0500

    backend records app

---
+++ db/conn.mjs
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+
+const connectionString = process.env.URI || "";
+
+const client = new MongoClient(connectionString);
+
+let conn;
+try {
+  conn = await client.connect();
+} catch(e) {
+  console.error(e);
+}
+
+let db = conn.db("inventory");
+
+export default db;
